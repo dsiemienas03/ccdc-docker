457ad0564930f5f392c7947c50eb1f2b929d49db
David Siemienas <david@siemienas.dev>
2025-02-04 23:16:47 -0600

    More improvements

--- src/scripts/vmware-setup.sh
+++ src/scripts/vmware-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash 
 read -p "ESXi Host IP: " esxi_host_ip
 read -p "ESXi Host Password: " esxi_host_pw
+read -p "Firewall Name: " fw_name
 
 
 
@@ -17,6 +18,7 @@ esxi:
     ${esxi_host_ip}:
       esxi_user: root
       esxi_password: ${esxi_host_pw}
+      fw_name: ${fw_name}
 
 EOF
 
